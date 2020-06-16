234ba3bef3a746bcb0beb0283167c309749910cb
Mike P. Sinn <m@quantimodo.com>
2020-06-16 14:21:10 -0500

    v6

--- dist/apis/index.js
+++ dist/apis/index.js
@@ -3,23 +3,4 @@ function __export(m) {
     for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
 }
 Object.defineProperty(exports, "__esModule", { value: true });
-__export(require("./ActivitiesApi"));
-__export(require("./AnalyticsApi"));
-__export(require("./AppSettingsApi"));
-__export(require("./AuthenticationApi"));
-__export(require("./ConnectorsApi"));
 __export(require("./DefaultApi"));
-__export(require("./FeedApi"));
-__export(require("./FriendsApi"));
-__export(require("./GroupsApi"));
-__export(require("./MeasurementsApi"));
-__export(require("./MessagesApi"));
-__export(require("./NotificationsApi"));
-__export(require("./RemindersApi"));
-__export(require("./SharesApi"));
-__export(require("./StudiesApi"));
-__export(require("./UnitsApi"));
-__export(require("./UploadApi"));
-__export(require("./UserApi"));
-__export(require("./VariablesApi"));
-__export(require("./XprofileApi"));
